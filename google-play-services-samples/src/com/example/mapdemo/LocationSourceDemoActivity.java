acaa9898a13dd5d81e7108fe50d0249f20970eae
maciek.gorski <maciek.gorski@gmail.com>
2013-03-14 09:58:01 +0100

    warnings

--- google-play-services-samples/src/com/example/mapdemo/LocationSourceDemoActivity.java
+++ google-play-services-samples/src/com/example/mapdemo/LocationSourceDemoActivity.java
@@ -24,7 +24,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.LocationSource;
-import com.google.android.gms.maps.LocationSource.OnLocationChangedListener;
 import com.google.android.gms.maps.model.LatLng;
 
 /**
