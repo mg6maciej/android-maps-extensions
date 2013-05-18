fbb71dd294c6ea9084220f3cc4a3ba23885e5fe9
maciek.gorski <maciek.gorski@gmail.com>
2013-05-18 18:09:38 +0200

    examples showing issues 14 and 15

---
+++ android-maps-extensions-demo/src/pl/mg6/android/maps/extensions/demo/Issue14InfoWindowNotShowingExampleActivity.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2013 Maciej Górski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pl.mg6.android.maps.extensions.demo;
+
+import pl.mg6.android.maps.extensions.ClusteringSettings;
+import pl.mg6.android.maps.extensions.GoogleMap;
+import pl.mg6.android.maps.extensions.SupportMapFragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Issue14InfoWindowNotShowingExampleActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.simple_map);
+
+		FragmentManager fm = getSupportFragmentManager();
+		SupportMapFragment f = (SupportMapFragment) fm.findFragmentById(R.id.map);
+		GoogleMap map = f.getExtendedMap();
+
+		ClusteringSettings settings = new ClusteringSettings();
+		settings.iconDataProvider(new DemoIconProvider(getResources()));
+		map.setClustering(settings);
+
+		MarkerOptions options = new MarkerOptions().position(new LatLng(0, 0)).title("title");
+		map.addMarker(options).showInfoWindow();
+	}
+}
